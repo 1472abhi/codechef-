0b778cfd83fbc701972d816b33deae487ca93780
Abhishek Mahale <96944806+1472abhi@users.noreply.github.com>
2022-11-18 06:01:24 +0530

    Add files via upload

---
+++ PRACLIST.java
@@ -0,0 +1,27 @@
+/* Problem
+
+CodeChef recently revamped its practice page to make it easier for users to identify the next problems they should solve by introducing some new features:
+
+    Recent Contest Problems - contains only problems from the last 2 contests
+    Separate Un-Attempted, Attempted, and All tabs
+    Problem Difficulty Rating - the Recommended dropdown menu has various difficulty ranges so that you can attempt the problems most suited to your experience
+    Popular Topics and Tags
+
+Our Chef is currently practicing on CodeChef and is a beginner. The count of ‘All Problems’ in the Beginner section is XXX. Our Chef has already ‘Attempted’ YYY problems among them. How many problems are yet ‘Un-attempted’?*/
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner (System.in);
+	    int x=sc.nextInt();
+	     int y=sc.nextInt();
+	 System.out.println(x-y);   
+	         
+	}
+}
